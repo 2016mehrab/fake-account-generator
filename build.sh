ef05b2ffd8819007d4a4ce7f7c984e430e39db9b
Eshan <2016mehrab@gmail.com>
2025-04-19 15:09:20 +0600

    chore(build): improve build process
    - Added npm install step
    - Improved dist folder cleanup
    - Added manifest.json and assets copy
    - Updated web-ext build command
    - Removed unused DOMPurify import

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+set -e
+
 npm i 
 
-rm -rf dist
+rm -vrf dist
 
 npx vite build
 
+cp manifest.json dist/
+cp -v icon* dist/  
+cp -v *.html dist/  
+cp -v *.css dist/  
+
+
+# npx web-ext build --ignore-files "*.http" "drawing.svg" --overwrite-dest
+npx web-ext build --source-dir=dist --overwrite-dest 
 echo "✅ Extension ready."
